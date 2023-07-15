aca59c9af77e8da4add556bd02e1e7c3970b3dc4
Riya Arora <arorariya2003@gmail.com>
2023-07-15 11:48:56 +0530

    set-up the server end points
    app.js by-default reads from the views directory, set up the index.ejs and channels.ejs pages inside of views

---
+++ app.js
@@ -0,0 +1,28 @@
+// Creating the Express App
+const express = require('express');
+const app = express();
+
+// Creating Server
+const http = require('http');
+const server = http.createServer(app);
+
+// Setting views
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/assets'));
+
+// Middleware for Form Posting
+app.use(express.urlencoded({extended : true}));
+
+// End-Points
+app.get('/', (req, res)=>{
+    res.render('index');
+})
+
+app.get('/channels', (req, res)=>{
+    res.render('channels');
+})
+
+const port = 3000;
+server.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
