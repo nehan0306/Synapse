39eddc2f953e536ff104cfb7df30ef8697261118
Riya Arora <arorariya2003@gmail.com>
2023-07-29 16:18:20 +0530

    chats modified

--- assets/js/development.js
+++ assets/js/development.js
@@ -10,19 +10,19 @@ const messageContainer = document.getElementById('list-chats');
 var audio = new Audio('audio/ting.mp3');
 
 // Function which will append event info to the contaner
-const append = (message, position)=>{
+const append = (usero, message, position)=>{
     const messageElement = document.createElement('li');
     messageElement.classList.add('d-flex');
     messageElement.classList.add('justify-content-between');
     messageElement.classList.add('mb-4');
     if (position == 'left'){
-    messageElement.innerHTML = `<img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"   class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60"> <div class="card mask-custom">    <div class="card-header d-flex justify-content-between p-3" style="border-bottom: 1px solid rgba(255,255,255,.3);">      <p class="fw-bold mb-0"> User</p>   <p class="text-light small mb-0"><i class="far fa-clock"></i> 12 mins ago</p> </div>  <div class="card-body"> <p class="mb-0">  ${message} </p> </div> </div>`;
+    messageElement.innerHTML = `<img src="img/profiler.png" alt="avatar"   class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60"> <div class="card mask-custom">    <div class="card-header d-flex justify-content-between p-3" style="border-bottom: 1px solid rgba(255,255,255,.3);">      <p class="fw-bold mb-0"> ${usero} </p>   <p class="text-light small mb-0"><i class="far fa-clock"></i> 12 mins ago</p> </div>  <div class="card-body"> <p class="mb-0">  ${message} </p> </div> </div>`;
     }
     else {
         messageElement.innerHTML = `<div class="card mask-custom w-100">
         <div class="card-header d-flex justify-content-between p-3"
           style="border-bottom: 1px solid rgba(255,255,255,.3);">
-          <p class="fw-bold mb-0">Lara Croft</p>
+          <p class="fw-bold mb-0">${usero}</p>
           <p class="text-light small mb-0"><i class="far fa-clock"></i> 13 mins ago</p>
         </div>
         <div class="card-body">
@@ -31,7 +31,7 @@ const append = (message, position)=>{
           </p>
         </div>
       </div>
-      <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+      <img src="img/profiler.png" alt="avatar"
         class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">`;
     }
     messageContainer.append(messageElement);
@@ -53,25 +53,25 @@ socket.emit('new-user-joined', name);
 // If a new user joins, receive his/her name from the server
 // No-longer being used
 socket.on('user-joined', name =>{
-    append(`${name} joined the chat`, 'right')
+    append(name, `${name} joined the chat`, 'right')
 })
 
 // If server sends a message, receive it
 socket.on('receive', data =>{
-    append(data.message, 'left')
+    append(data.name, data.message, 'left')
 })
 
 // If a user leaves the chat, append the info to the container
 // No-longer being used
 socket.on('left', name =>{
-    append(`${name} left the chat`, 'left');
+    append(name, `${name} left the chat`, 'left');
 })
 
 // If the form gets submitted, send server the message
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
-    append(`You : ${message}`, 'right');
+    append('You', message, 'right');
     socket.emit('send', message);
     messageInput.value = ''
 })
