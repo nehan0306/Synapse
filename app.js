5f1fe7f7cfca162fad977e502e4763dc3adbea9f
Riya Arora <arorariya2003@gmail.com>
2023-07-29 16:06:46 +0530

    chats-format changed

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ io.on('connection', socket =>{
     // If any new user joins, let other users connected to the server know!
     socket.on('new-user-joined', name =>{ 
         users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
+        // socket.broadcast.emit('user-joined', name);
     });
 
     // If someone sends a message, broadcast it to other people
@@ -79,7 +79,7 @@ io.on('connection', socket =>{
 
     // If someone leaves the chat, let others know 
     socket.on('disconnect', message =>{
-        socket.broadcast.emit('left', users[socket.id]);
+        // socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
 })
--- assets/js/development.js
+++ assets/js/development.js
@@ -2,19 +2,45 @@ var socket = io.connect('http://localhost:3000');
 
 // Get DOM elements in respective Js variables
 const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp')
-const messageContainer = document.querySelector(".container")
+const messageInput = document.getElementById('messageInp');
+// const messageContainer = document.querySelector(".container")
+const messageContainer = document.getElementById('list-chats');
 
 // Message Audio
 var audio = new Audio('audio/ting.mp3');
 
 // Function which will append event info to the contaner
 const append = (message, position)=>{
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
+    const messageElement = document.createElement('li');
+    messageElement.classList.add('d-flex');
+    messageElement.classList.add('justify-content-between');
+    messageElement.classList.add('mb-4');
+    if (position == 'left'){
+    messageElement.innerHTML = `<img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-6.webp" alt="avatar"   class="rounded-circle d-flex align-self-start me-3 shadow-1-strong" width="60"> <div class="card mask-custom">    <div class="card-header d-flex justify-content-between p-3" style="border-bottom: 1px solid rgba(255,255,255,.3);">      <p class="fw-bold mb-0"> User</p>   <p class="text-light small mb-0"><i class="far fa-clock"></i> 12 mins ago</p> </div>  <div class="card-body"> <p class="mb-0">  ${message} </p> </div> </div>`;
+    }
+    else {
+        messageElement.innerHTML = `<div class="card mask-custom w-100">
+        <div class="card-header d-flex justify-content-between p-3"
+          style="border-bottom: 1px solid rgba(255,255,255,.3);">
+          <p class="fw-bold mb-0">Lara Croft</p>
+          <p class="text-light small mb-0"><i class="far fa-clock"></i> 13 mins ago</p>
+        </div>
+        <div class="card-body">
+          <p class="mb-0">
+            ${message}
+          </p>
+        </div>
+      </div>
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-5.webp" alt="avatar"
+        class="rounded-circle d-flex align-self-start ms-3 shadow-1-strong" width="60">`;
+    }
     messageContainer.append(messageElement);
+
+    // const messageElement = document.createElement('div');
+    // messageElement.innerText = message;
+    // messageElement.classList.add('message');
+    // messageElement.classList.add(position);
+    // messageContainer.append(messageElement);
     if(position =='left'){ 
         audio.play();
     }
@@ -25,16 +51,18 @@ const name = prompt("Please enter your preferred name");
 socket.emit('new-user-joined', name);
 
 // If a new user joins, receive his/her name from the server
+// No-longer being used
 socket.on('user-joined', name =>{
     append(`${name} joined the chat`, 'right')
 })
 
 // If server sends a message, receive it
 socket.on('receive', data =>{
-    append(`${data.name}: ${data.message}`, 'left')
+    append(data.message, 'left')
 })
 
 // If a user leaves the chat, append the info to the container
+// No-longer being used
 socket.on('left', name =>{
     append(`${name} left the chat`, 'left');
 })
@@ -43,7 +71,7 @@ socket.on('left', name =>{
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
+    append(`You : ${message}`, 'right');
     socket.emit('send', message);
     messageInput.value = ''
 })
